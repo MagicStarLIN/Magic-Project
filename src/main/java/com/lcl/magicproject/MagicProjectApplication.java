1436c8bd0bb3e22ace8b0c3940aa7c2c3c908c62
刘常林 <751969316@qq.com>
2025-02-19 01:46:58 +0800

    first commit

---
+++ src/main/java/com/lcl/magicproject/MagicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lcl.magicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicProjectApplication.class, args);
+    }
+
+}
